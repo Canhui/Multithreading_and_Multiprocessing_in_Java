cc3c4ca6a8b9630aaeb16f3bef28ed18dac788f5
Canhui WANG <Canhui@users.noreply.github.com>
2019-03-07 14:25:24 +0800

    Update Program1_1.java Code

--- Program1_1.java
+++ Program1_1.java
@@ -10,14 +10,14 @@ public class Program1_1 {
 		Taylorseries1 t1 = new Taylorseries1();
 		Taylorseries2 t2 = new Taylorseries2();
 		
-		double sin_taylorseries = t1.Sin(x, series);
-		double cos_taylorseries = t2.Cos(x, series);
+		t1.Sin(x, series);
+		t2.Cos(x, series);
 		
 		long end_time = System.nanoTime();
 		double cost_time = (end_time - begin_time)/(1000000000.0000);
 
-		System.out.println("sin(" + x + ") ~= " + sin_taylorseries);
-		System.out.println("cos(" + x + ") ~= " + cos_taylorseries);
+		
+//		System.out.println("cos(" + x + ") ~= " + cos_taylorseries);
 		System.out.printf("Time used for computing -> "+ series +" Taylor series of sin & cos functions = "+ cost_time + " (secs)");
 	}
 
@@ -25,12 +25,13 @@ public class Program1_1 {
 
 class Taylorseries1 {
 	
-	public static double Sin(double x, int n) {
+	public static void Sin(double x, int n) {
 		double sin_approx = 0;
 		for (int i=0; i<n; i++) {
 			sin_approx += Math.pow(-1, i) * Math.pow(x, 2*i+1) * Invfactorial(2*i+1);
 		}
-		return sin_approx;
+		System.out.println("sin(" + x + ") ~= " + sin_approx);
+//		return sin_approx;
 	}
 	
 	public static double Invfactorial(int n) {
@@ -50,12 +51,13 @@ class Taylorseries1 {
 
 class Taylorseries2 {
 	
-	public static double Cos(double x, int n) {
+	public static void Cos(double x, int n) {
 		double cos_approx = 0;
 		for (int i=0; i<n; i++) {
 			cos_approx += Math.pow(-1, i) * Math.pow(x, 2*i) * Invfactorial(2*i);
 		}
-		return cos_approx;
+		System.out.println("cos(" + x + ") ~= " + cos_approx);
+//		return cos_approx;
 	}
 	
 	public static double Invfactorial(int n) {
